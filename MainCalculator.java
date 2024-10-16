e1c1a39c111abcd6905c25371c03fd89319f8fb8
Zyad Kherraf <131729805+KHZyad@users.noreply.github.com>
2024-10-16 18:10:04 +0100

    Add files via upload
    this is the main class by Zyad

---
+++ MainCalculator.java
@@ -0,0 +1,36 @@
+public class MainCalculator {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: java mySoftware <operator> <A> <B>");
+            return;
+        }
+
+        String operator = args[0];
+        double A = Double.parseDouble(args[1]);
+        double B = Double.parseDouble(args[2]);
+
+        Calculator calculator = null;
+
+        switch (operator) {
+            case "+":
+                calculator = new Addition();
+                break;
+            case "-":
+                calculator = new Subtraction();
+                break;
+            case "*":
+                calculator = new Multiplication();
+                break;
+            case "/":
+                calculator = new Division();
+                break;
+            default:
+                System.out.println("Unknown operator: " + operator);
+                return;
+        }
+
+        // Compute and display the result
+        double result = calculator.compute(A, B);
+        System.out.println("Result: " + result);
+    }
+}
